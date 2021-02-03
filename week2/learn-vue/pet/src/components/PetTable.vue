ae54259d234c7b668dbbefa8d032e880c91a7cd8
Tthienphuc147 <43287192+Tthienphuc147@users.noreply.github.com>
2021-02-03 17:42:11 +0700

    --update

--- week2/learn-vue/pet/src/components/PetTable.vue
+++ week2/learn-vue/pet/src/components/PetTable.vue
@@ -1,29 +1,19 @@
 <template>
   <div>
     <h1>{{ species }} for Adoption</h1>
-    <!-- <table class="table">
-      <thead>
-        <th>Name</th>
-        <th>Breed</th>
-        <th>Gender</th>
-        <th>Age</th>
-      </thead>
-      <tbody>
-        <tr v-for="(item,index) in pets" v-bind:key="index">
-          <td> <router-link :to="`/pets/${species}/${index}`">{{item.name}}</router-link></td>
-          <td>{{item.breed}}</td>
-          <td>{{item.gender}}</td>
-          <td>{{item.age}}</td>
-        </tr>
-      </tbody>
-    </table> -->
     <div class="row">
-      <div class="col-md-4" v-for="(item,index) in pets" v-bind:key="index">
+      <div class="col-md-2" v-for="(item,index) in pets" v-bind:key="index">
         <div class="card">
-          <p>Name: {{item.name}}</p>
-          <p>Breed: {{item.breed}}</p>
-          <p>Gender: {{item.gender}}</p>
-          <p>Age: {{item.age}}</p>
+          <div class="image">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSp2_ZZeZG3XdM_bFH_dL0Fm-ijvQhylGEEPA&usqp=CAU" alt="">
+          </div>
+          <div class="quote text-left d-flex flex-column">
+            <span class="mb-1">Name: {{item.name}}</span>
+            <span class="mb-1">Breed: {{item.breed}}</span>
+            <span class="mb-1">Gender: {{item.gender}}</span>
+            <span class="mb-1">Age: {{item.age}}</span>
+          </div>
+<router-link :to="`/pets/${species}/${index}`">          <button class="btn btn-sm btn-info">Go detail</button></router-link>
         </div>
       </div>
     </div>
@@ -38,4 +28,17 @@ export default {
   }
 }
 </script>
-styl
+<style lang="scss" scoped>
+ .card {
+   display: flex;
+   justify-content: between;
+   padding:10px;
+   margin-bottom:10px;
+   .image {
+     width: 100%;
+     img {
+       width: 100%;
+     }
+   }
+ }
+</style>
