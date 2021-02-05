710f2ee10c05e0e673910e7cd8b8c6466f96143c
Tthienphuc147 <43287192+Tthienphuc147@users.noreply.github.com>
2021-02-05 18:03:18 +0700

    --update

--- week2/learn-vue/vue-demo/src/router/guard-service.js
+++ week2/learn-vue/vue-demo/src/router/guard-service.js
@@ -1,5 +1,5 @@
 export default (to, from, next) => {
-  if (localStorage.getItem('meetUpKey')) {
+  if (localStorage.getItem("meetUpKey")) {
     next();
   } else {
     next("/account/sign-in");
--- week2/learn-vue/vue-demo/src/store/index.js
+++ week2/learn-vue/vue-demo/src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import firebase from 'firebase'
+import firebase from "firebase";
 import router from "../router";
 Vue.use(Vuex);
 
@@ -9,164 +9,177 @@ export default new Vuex.Store({
     meetUpsData: [],
     user: null,
     loading: false,
-    error: null
+    error: null,
   },
   mutations: {
-    setMeetups (state, payload) {
+    setMeetups(state, payload) {
       state.meetUpsData = payload;
     },
-    createMeetup: (state,payload) => {
+    createMeetup: (state, payload) => {
       state.meetUpsData.push(payload);
       router.push("/meet-up/meet-up-list");
     },
-    setUser (state, payload) {
-      state.user = payload,
-      payload && localStorage.setItem('meetUpKey',JSON.stringify({
-        email: payload.email,
-        isAuthenticated:true
-      }));
+    setUser(state, payload) {
+      (state.user = payload),
+        payload &&
+          localStorage.setItem(
+            "meetUpKey",
+            JSON.stringify({
+              email: payload.email,
+              isAuthenticated: true,
+            })
+          );
       router.push("/");
     },
-    setLoading (state, payload) {
-      state.loading = payload
+    setLoading(state, payload) {
+      state.loading = payload;
     },
-    setError (state, payload) {
-      state.error = payload
+    setError(state, payload) {
+      state.error = payload;
     },
-    clearError (state) {
-      state.error = null
-    }
+    clearError(state) {
+      state.error = null;
+    },
   },
   actions: {
-    loadMeetUps ({ commit}) {
-      firebase.database().ref('meetups').once('value').then(item => {
-        const data = [];
-        const obj = item.val();
-        for (const key in obj) {
-          data.push({
-            id: key,
-            title: obj[key].title,
-            description: obj[key].description,
-            imageUrl: obj[key].imageUrl,
-            date: obj[key].date
-          })
-        }
-        commit('setMeetups', data)
-      })
+    loadMeetUps({ commit }) {
+      firebase
+        .database()
+        .ref("meetups")
+        .once("value")
+        .then((item) => {
+          const data = [];
+          const obj = item.val();
+          for (const key in obj) {
+            data.push({
+              id: key,
+              title: obj[key].title,
+              description: obj[key].description,
+              imageUrl: obj[key].imageUrl,
+              date: obj[key].date,
+            });
+          }
+          commit("setMeetups", data);
+        });
     },
-    createMeetup ({commit, getters}, payload) {
+    createMeetup({ commit, getters }, payload) {
       const meetup = {
         title: payload.title,
         location: payload.location,
         description: payload.description,
         date: payload.date,
-        creatorId: getters.user.id
-      }
-      let key = '';
-      firebase.database().ref('meetups').push(meetup)
+        creatorId: getters.user.id,
+      };
+      let key = "";
+      firebase
+        .database()
+        .ref("meetups")
+        .push(meetup)
         .then((data) => {
-          key = data.key
-          return key
+          key = data.key;
+          return key;
         })
-        .then(key => {
-          const filename = payload.image.name
-          const ext = filename.slice(filename.lastIndexOf('.'))
-          return firebase.storage().ref('meetups/' + key + ext).put(payload.image)
+        .then((key) => {
+          const filename = payload.image.name;
+          const ext = filename.slice(filename.lastIndexOf("."));
+          return firebase
+            .storage()
+            .ref("meetups/" + key + ext)
+            .put(payload.image);
         })
         .then((fileData) => {
-          return fileData.ref.getDownloadURL()
+          return fileData.ref.getDownloadURL();
         })
-        .then(res => {firebase.database().ref('meetups').child(key).update({imageUrl: res})
-        commit('createMeetup', {
-          ...meetup,
-          imageUrl: res,
-          id: key
+        .then((res) => {
+          firebase
+            .database()
+            .ref("meetups")
+            .child(key)
+            .update({ imageUrl: res });
+          commit("createMeetup", {
+            ...meetup,
+            imageUrl: res,
+            id: key,
+          });
+          Vue.notify({
+            group: "notification",
+            title: "Notification",
+            type: "success",
+            text: "Create meetup success",
+          });
         })
-        Vue.notify({
-          group: 'notification',
-          title: 'Notification',
-          type: 'success',
-          text: 'Create meetup success'
-        })
-      })
         .catch((err) => {
           Vue.notify({
-            group: 'notification',
-            title: 'Notification',
-            type: 'error',
-            text: err.message
-          })
+            group: "notification",
+            title: "Notification",
+            type: "error",
+            text: err.message,
+          });
+        });
+    },
+    signUserUp({ commit }, payload) {
+      commit("setLoading", true);
+      commit("clearError");
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(payload.email, payload.password)
+        .then((user) => {
+          commit("setLoading", false);
+          const newUser = {
+            id: user.uid,
+          };
+          commit("setUser", newUser);
+          Vue.notify({
+            group: "notification",
+            title: "Notification",
+            type: "success",
+            text: "Sign up complete",
+          });
         })
+        .catch((error) => {
+          commit("setLoading", false);
+          commit("setError", error);
+          Vue.notify({
+            group: "notification",
+            title: "Notification",
+            type: "error",
+            text: error.message,
+          });
+        });
     },
-    signUserUp ({commit}, payload) {
-      commit('setLoading', true)
-      commit('clearError')
-      firebase.auth().createUserWithEmailAndPassword(payload.email, payload.password)
-        .then(
-          user => {
-            commit('setLoading', false)
-            const newUser = {
-              id: user.uid,
-              
-            }
-            commit('setUser', newUser)
-            Vue.notify({
-              group: 'notification',
-              title: 'Notification',
-              type: 'success',
-              text: 'Sign up complete'
-            })
-          }
-        )
-        .catch(
-          error => {
-            commit('setLoading', false)
-            commit('setError', error)
-            Vue.notify({
-              group: 'notification',
-              title: 'Notification',
-              type: 'error',
-              text: error.message
-            })
-          }
-        )
+    signUserIn({ commit }, payload) {
+      commit("setLoading", true);
+      commit("clearError");
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(payload.email, payload.password)
+        .then((user) => {
+          const newUser = {
+            id: user.user.uid,
+          };
+          commit("setUser", newUser);
+          Vue.notify({
+            group: "notification",
+            title: "Notification",
+            type: "success",
+            text: "Sign in success",
+          });
+        })
+        .catch((error) => {
+          commit("setLoading", false);
+          commit("setError", error);
+          Vue.notify({
+            group: "notification",
+            title: "Notification",
+            type: "error",
+            text: error.message,
+          });
+        });
     },
-    signUserIn ({commit}, payload) {
-      commit('setLoading', true)
-      commit('clearError')
-      firebase.auth().signInWithEmailAndPassword(payload.email, payload.password)
-        .then(
-          user => {
-            const newUser = {
-              id: user.user.uid,
-            }
-            commit('setUser', newUser)
-            Vue.notify({
-              group: 'notification',
-              title: 'Notification',
-              type: 'success',
-              text: 'Sign in success'
-            })
-          }
-          
-        )
-        .catch(
-          error => {
-            commit('setLoading', false)
-            commit('setError', error)
-            Vue.notify({
-              group: 'notification',
-              title: 'Notification',
-              type: 'error',
-              text: error.message
-            })
-          }
-        )
-    },
-    logout ({commit}) {
-      firebase.auth().signOut()
-      localStorage.removeItem('meetUpKey')
-      commit('setUser', null)
+    logout({ commit }) {
+      firebase.auth().signOut();
+      localStorage.removeItem("meetUpKey");
+      commit("setUser", null);
     },
   },
   getters: {
@@ -174,17 +187,17 @@ export default new Vuex.Store({
       return state.meetUpsData;
     },
     getDetailMeetUp: (state) => (id) => {
-      return state.meetUpsData.find(item => item.id === id)
+      return state.meetUpsData.find((item) => item.id === id);
     },
-    user (state) {
-      return state.user
+    user(state) {
+      return state.user;
     },
-    loading (state) {
-      return state.loading
+    loading(state) {
+      return state.loading;
     },
-    error (state) {
-      return state.error
-    }
+    error(state) {
+      return state.error;
+    },
   },
-  modules: {}
+  modules: {},
 });
